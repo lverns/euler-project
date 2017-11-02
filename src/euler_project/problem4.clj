ae8a959367c9c9180d6a4f57fe29740312fa660d
Laverne Schrock <laverne@schrock.email>
2017-11-02 16:14:48 -0500

    Implement unoptimized solution to Problem 4
    A couple of places for potential improvement.
    
    - We convert the number to a string to test whether it is a palendrome.
    - Since the results from the list comprehension are not sorted, we have
      to compute _all_ the products.
    - (apply max result) means we have to compute all the products and do
      the palindrome test for each one _before_ max starts computing.

--- src/euler_project/problem4.clj
+++ src/euler_project/problem4.clj
@@ -14,4 +14,22 @@
   (= s (apply str (reverse s))))
 
 
+(defn reverse-range
+  "Produces a lazy list containing all the integers from high to low (both inclusive),
+   when high is greater than or equal to low."
+  [high low]
+  (cond
+    (< high low) '()
+    (= high low) (list low)
+    :else (lazy-seq (cons high
+                             (reverse-range (dec high) low)))))
 
+(def result
+  (for [x (reverse-range 999 100)
+        y (reverse-range 999 100)
+        :when (palindrome? (str (* x y)))]
+    (* x y)))
+
+(def solution (apply max result))
+solution
+;; => 906609
