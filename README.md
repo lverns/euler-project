d13169edc0a4ca6d67bd670f5eedeef4f96a1c40
Laverne Schrock <laverne@schrock.email>
2017-11-21 12:34:01 -0600

    Add profiles and aliases to ease running of solutions
    Also update README.md to reflect this change.

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 A Clojure project to hold my solutions to select problems from the Euler Project.
 
-To run a problem, add `:main euler-project.problemN` to the project definition in
-`project.clj`.
+Problems can be run with `lein problemN`.
 
 ## License
 
--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject euler-project "0.1.0-SNAPSHOT"
   :description "A project for holding solutions to some Euler Project problems"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:problem4 {:main euler-project.problem4}
+             :problem8 {:main euler-project.problem8}}
+  :aliases {"problem4" ["with-profile" "problem4" "run"]
+            "problem8" ["with-profile" "problem8" "run"]})
