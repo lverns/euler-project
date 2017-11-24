c3a105886d6c6fe245c0368bb49982066317574e
Laverne Schrock <laverne@schrock.email>
2017-11-24 12:24:16 -0600

    Add dependency on math.numeric-tower

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject euler-project "0.1.0-SNAPSHOT"
   :description "A project for holding solutions to some Euler Project problems"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles {:problem4 {:main euler-project.problem4}
              :problem7 {:main euler-project.problem7}
              :problem8 {:main euler-project.problem8}}
