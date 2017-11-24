634288e1c5cd6829915e95392c2527830f54c082
Laverne Schrock <laverne@schrock.email>
2017-11-24 09:43:33 -0600

    Create empty template for Problem 7

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "A project for holding solutions to some Euler Project problems"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:problem4 {:main euler-project.problem4}
+             :problem7 {:main euler-project.problem7}
              :problem8 {:main euler-project.problem8}}
   :aliases {"problem4" ["with-profile" "problem4" "run"]
+            "problem7" ["with-profile" "problem7" "run"]
             "problem8" ["with-profile" "problem8" "run"]})
